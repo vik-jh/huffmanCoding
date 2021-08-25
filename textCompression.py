2a5681c71f509990d289b32b9f8b6f738e9faed9
Vikash Jha <jha2020vikash@yahoo.com>
2021-08-25 17:42:03 +0545

    additional changes made

--- textCompression.py
+++ textCompression.py
@@ -8,10 +8,10 @@ class BinaryTreeNode:
         self.right = None
 
     def __lt__(self, other):
-        return self.frequency < other.frequency
+        return self.freq < other.freq
 
     def __eq__(self, other):
-        return self.frequency == other.frequency
+        return self.freq == other.freq
 
 
 
@@ -20,6 +20,7 @@ class HuffmanCoding:
     def __init__(self, path):
         self.path = path
         self.__heap = []
+        self.__codes = {}
 
 
     def __make_frequency_dict(self, text):
@@ -38,9 +39,70 @@ class HuffmanCoding:
             binary_tree_node = BinaryTreeNode(key, frequency)
             heapq.push(self.__heap, binary_tree_node)
 
+    def __buildTree(self):
+        while(len(self.__heap) > 1):
+            binary_tree_node_1 = heapq.heappop(self.__heap)
+            binary_tree_node_2 = heapq.heappop(self.__heap)
+            freq_sum = binary_tree_node_1.freq + binary_tree_node_2.freq
+
+            newNode  = BinaryTreeNode(None, freq_sum)
+            newNode.left = binary_tree_node_1
+            newNode.right = binary_tree_node_2
+
+            heapq.heappush(self.__heap, newNode)
+
+        return
+
+    def __buildCodesHelper(self, root, curr_bits):
+
+        if root is None:
+            return 
+
+        if root.value is not None:
+            self.__codes[root.value] = curr_bits
+            return 
 
 
 
+        self.__buildCodesHelper(self, root.left, curr_bits + "0")
+        self.__buildCodesHelper(self, root.right, curr_bits + "1")
+
+
+    def __buildCodes(self):
+        root = heapq.heappop(self.__heap)
+        self.__buildCodesHelper(root, "")
+
+
+    def __getEncodedText(self, text):
+        encoded_text = ""
+        for char in text:
+            encoded_text += self.__codes[char]
+        return encoded_text
+
+
+    def __getPaddedEncodedText(self, encoded_text):
+
+        padded_amount = 8 - (len(encoded_text))
+
+        for i in range(padded_amount):
+            encoded_text += '0'
+
+        padded_info = {"0:08b"}.format(padded_amount)
+        padded_encoded_text = padded_info + encoded_text
+        return padded_encoded_text
+
+
+
+    def __getBytesArray(self, padded_encoded_text):
+
+        array = []
+
+        for i in range(0, len(padded_encoded_text), 8):
+            byte = padded_encoded_text[i : i+8]
+            array.append(int(byte, 2))
+
+
+        return array
 
 
 
@@ -54,19 +116,39 @@ class HuffmanCoding:
         #make frequency dictionary using the text
 
         freq_dict = self.__make_frequency_dict(text)
+        
 
 
         #construct the heap from the frequency_dict
 
+        self.__buildHeap(freq_dict)
+
         #construct the binary tree from the heap 
+
+        self.__buildTree()
 
         # construct the codes from binary tree
 
+        self.__buildCodes()
+
+
         #create encoded text using the codes
+        encoded_text = self.__getEncodedText(text)
+
+        #padding encoded Text
+
+        padded_encoded_text = self.__getPaddedEncodedText(text)
+
+        bytes_array = self.__getBytesArray(padded_encoded_text)
 
         #put this encoded text into the binary file
 
-        #return this binary file
+
+
+        #return this binary file as output
+
+        final_bytes = bytes(bytes_array)
+        
 
 
         
